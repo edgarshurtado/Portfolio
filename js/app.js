f479aae8e89f5588631f03d53a73fb20e54c5021
Edgar S. Hurtado <edsanhu@gmail.com>
2017-08-05 16:28:36 +0200

    Add collapse logic to a btn

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
 const sections = document.querySelectorAll('.section')
+const closeBtns = document.querySelectorAll('.collapse-btn')
 
 sections.forEach((section) => {
     section.addEventListener('click', function(){
         this.classList.add('expanded')
     });
+})
+
+
+closeBtns.forEach(btn => {
+    btn.addEventListener('click', function(e){
+        e.stopPropagation()
+        this.parentElement.classList.remove('expanded')
+    })
 })
