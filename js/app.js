2853213f4da55c94397dd751fba77d4cb108f43f
Edgar S. Hurtado <edsanhu@gmail.com>
2017-08-06 12:54:02 +0200

    Add cards transitions relative with header section

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
+
 const sections = document.querySelectorAll('.section')
 const closeBtns = document.querySelectorAll('.collapse-btn')
+const headerSection = document.querySelector('.header')
+const sectionsDiv = document.querySelector('.sections')
 
 sections.forEach((section) => {
     section.addEventListener('click', function(){
         this.classList.add('expanded')
+        sectionsDiv.classList.add('expanded')
+        headerSection.classList.add('collapsed')
     });
 })
 
@@ -12,5 +17,7 @@ closeBtns.forEach(btn => {
     btn.addEventListener('click', function(e){
         e.stopPropagation()
         this.parentElement.classList.remove('expanded')
+        headerSection.classList.remove('collapsed')
+        sectionsDiv.classList.remove('expanded')
     })
 })
