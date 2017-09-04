0ea3d79b92268b998c9ab1bc6f43696068b8799c
Edgar S. Hurtado <edsanhu@gmail.com>
2017-09-04 20:00:35 +0200

    Sanatize post input

--- backend/mailer.php
+++ backend/mailer.php
@@ -1,9 +1,18 @@
 <?php
-$to = "edsanhu@gmail.com"; // this is your Email address
-$from = $_POST['email']; // this is the sender's Email address
+
+$sanitizeOptions = array(
+    'email' => FILTER_SANITIZE_EMAIL,
+    'name' =>  FILTER_SANITIZE_STRING,
+    'message' =>  FILTER_SANITIZE_STRING
+);
+
+$sanitizedPostData = filter_var_array($_POST, $sanitizeOptions);
+
+$to = "edsanhu@gmail.com";
+$from = $sanitizedPostData['email'];
 $serverMail = 'system@edgarsh.es';
-$name = $_POST['name'];
-$rawMessage = $_POST['message'];
+$name = $sanitizedPostData['name'];
+$rawMessage = $sanitizedPostData['message'];
 
 $subject = "Form submission";
 $subject2 = "Copy of your form submission";
@@ -15,6 +24,7 @@ $header = "From: $serverMail";
 
 mail($to,$subject,$message,$header);
 mail($from,$subject2,$message2,$header); // sends a copy of the message to the sender
+
 echo "Thank you for your message";
 header('refresh:2;url=http://edgarsh.es/projects/portfolio-demo');
 
