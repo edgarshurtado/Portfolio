b81c74586bdf4d66cffc0bcab82169cea231f40e
Edgar S. Hurtado <edsanhu@gmail.com>
2017-09-12 20:43:10 +0200

    Implement formChecking using Symbols

--- js/form-submission.js
+++ js/form-submission.js
@@ -1,10 +1,12 @@
 const form = document.querySelector('#contact-form')
-const nameInput = document.querySelector('#name')
-const emailInput = document.querySelector('#email')
-const messageInput = document.querySelector('#message')
 
+const nameInput = document.querySelector('#name')
 const nameFormGroup = document.querySelector('#name-form-group')
+
+const emailInput = document.querySelector('#email')
 const emailFormGroup = document.querySelector('#email-form-group')
+
+const messageInput = document.querySelector('#message')
 const messageFormGroup = document.querySelector('#message-form-group')
 
 const formGroupsToValidate = [nameFormGroup, emailFormGroup, messageFormGroup]
@@ -12,45 +14,51 @@ const formGroupsToValidate = [nameFormGroup, emailFormGroup, messageFormGroup]
 const emptyInput = Symbol('emptyInput')
 const filledInput = Symbol('filledInput')
 
-form.addEventListener('submit', (e) => {
-    debugger
+const errorsTargetLabels = {
+	[emptyInput]: 'label'
+}
+
+form.addEventListener('submit', submitHandler)
+
+function submitHandler(e){
     e.preventDefault()
 
-    const formChecks = formCheck()
+    const formChecksErrorsWeakMap = getFormErrorsWeakMap()
 
     for(const formGroup of formGroupsToValidate){
-        notifyError(formGroup, error)
+        notifyErrors(formGroup, formChecksErrorsWeakMap.get(formGroup))
     }
+}
 
-    console.log('No no no nooo, don\'t post this fooorm')
-})
+function notifyErrors(formGroup, errors){
 
-function notifyError(formGroup, error){
-    formGroup.querySelector('label').classList
-        .toggle('hidden', error.findIndex(emptyInput) >= 0 )
+	for(const errorSymbol of Reflect.ownKeys(errorsTargetLabels)){
+		formGroup.querySelector(errorsTargetLabels[errorSymbol]).classList
+			.toggle('hidden', errors.findIndex((error) => error === errorSymbol) === -1 )
+	}
 }
 
 
-function formCheck(){
-    let errors = new WeakMap([
+function getFormErrorsWeakMap(){
+    let errorsMap = new WeakMap([
         [nameFormGroup, []],
         [emailFormGroup, []],
         [messageFormGroup, []]
     ])
 
     if(!nameIsFilled()){
-        errors.set(nameFormGroup, [emptyInput])
+        errorsMap.set(nameFormGroup, [emptyInput])
     }
 
     if(!emailIsFilled()){
-        errors.set(emailFormGroup, [emptyInput])
+        errorsMap.set(emailFormGroup, [emptyInput])
     }
 
     if(!messageIsFilled()){
-        errors.set(messageFormGroup, [emptyInput])
+        errorsMap.set(messageFormGroup, [emptyInput])
     }
 
-    return errors
+    return errorsMap
 }
 
 function nameIsFilled(){
@@ -70,5 +78,5 @@ function messageIsFilled(){
 }
 
 function domInputElementIsFilled(domElement){
-    return nameInput.value.length !== 0
+    return domElement.value.length !== 0
 }
